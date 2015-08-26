98376e54e8bd27764ef5e685a192e02e3c71b48f
AliceDawoonChoi <dalmaab_alice@hotmail.com>
2015-08-26 12:25:16 -0300

    commit 2 (not final)

--- app/src/main/java/com/example/dawoon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dawoon/myapplication/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.dawoon.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -39,6 +44,9 @@ public class MainActivity extends Activity implements Observer {
     private ArrayList<Array1> mArrayList;
     private ListView lv;
     private CustomAdapter adapter;
+    private String firstkey1;
+    private String secondkey2;
+    private int id_DB;
 
 
     @Override
@@ -91,17 +99,47 @@ public class MainActivity extends Activity implements Observer {
 //                SearchResult.add(id, list);
 //                SearchResult.add(firstKey + " VS " + secondKey + " : " + result1 + " VS " + result2);
 
-                mArrayList.add(new Array1(firstKey,secondKey, result1, result2));
-                adapter = new CustomAdapter(this, mArrayList);
-                lv = (ListView)findViewById(id.result_ListView);
-                lv.setAdapter(adapter);
+                mArrayList.add(new Array1(firstKey, secondKey, result1, result2));
+
             }
             cursor.close();
 
-//            adapter = new CustomAdapter(this, mArrayList);
-//            lv = (ListView)findViewById(id.result_ListView);
-//            lv.setAdapter(adapter);
+            adapter = new CustomAdapter(this, mArrayList);
+            lv = (ListView)findViewById(id.result_ListView);
+            lv.setAdapter(adapter);
 
+
+            /////////////////////////test
+
+            lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    // 쭉 눌렀을 때 다이얼로그 창이 떠야함.
+                    Toast.makeText(getApplicationContext(), (position + 1) + "까꿍", Toast.LENGTH_SHORT).show();
+
+                    return false;
+                }
+//                @Override
+//            public boolean onMenuItemLongClick(MenuItem item){
+//                    switch (item.getItemId()) {
+//                        case id.linear_firstkey_searchBtn: // 롱클릭시 첫번째 검색어 사이트 이동버튼
+//                            break;
+//                        case id.linear_secondkey_serachBtn: //롱클릭시 두번째 검색어 사이트 이동버튼
+//                            break;
+//                        case id.linear_B_EditBTn: // 수정 버튼 : db를 지우고 화면 텍스트에 보여준다
+//
+//                            DatabaseConnector databaseConnector;
+//                            databaseConnector = new DatabaseConnector();
+//
+//                            databaseConnector.deleteContact();
+//                            break;
+//                        case id.linear_B_DeleteBtn: // 삭제 버튼 : db를 지운다. 화면 텍스트 아무것도 안보여줌.
+//                            DatabaseConnector databaseConnector;
+//                            databaseConnector = new DatabaseConnector(this);
+//                            databaseConnector.deleteContact();
+//                            break;
+//                    }
+            });
 
 //            // 어댑터 준비
 //            resultAdapter = new ArrayAdapter<String>(this, layout.layout_result, SearchResult);
@@ -131,6 +169,7 @@ public class MainActivity extends Activity implements Observer {
 //
 //            textview_FirstKeyword.setText(String.valueOf(firstKeyword));
 //            textview_SecondKeyword.setText(String.valueOf(secondKeyword));
+//
             // 검색 키 관련 끝
         } catch (Exception e){
             e.toString();
@@ -179,18 +218,24 @@ public class MainActivity extends Activity implements Observer {
                 holder = (ViewHolder) converView.getTag();
             }
             holder.a.setText(mArrayList.get(position).firstkeyword);
-            holder.a.setText(mArrayList.get(position).secondkeyword);
-            holder.a.setText(mArrayList.get(position).result1);
-            holder.a.setText(mArrayList.get(position).result2);
+            holder.b.setText(mArrayList.get(position).secondkeyword);
+            holder.c.setText(String.valueOf(mArrayList.get(position).result1));
+            holder.d.setText(String.valueOf(mArrayList.get(position).result2));
             return converView;
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//         Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.test, menu);
+//        super.onCreateContextMenu();
         return true;
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo m){
+        getMenuInflater().inflate(R.menu.test, menu);
+        super.onCreateContextMenu(menu, v, m);
     }
 
     @Override
@@ -212,21 +257,38 @@ public class MainActivity extends Activity implements Observer {
 
     public void compareOnClick(View v) throws IOException {
 
-        EditText text_FirstKeyword = null;
-        text_FirstKeyword = (EditText) findViewById(id.firstKey);
-        EditText text_SecondKeyword = null;
-        text_SecondKeyword = (EditText) findViewById(id.secondKey);
+        switch (v.getId()){
+//            case R.id.btn1 : // 다이얼로그 버튼
+//                MAinDialog1 d = new MAinDialog1("","",0,0);
+//                d.dismiss();
+//                break;
+            case id.result_ListView : // 리스트뷰
 
-        String firstKeyword = text_FirstKeyword.getText().toString();
-        String secondkeyword = text_SecondKeyword.getText().toString();
+                break;
+            case id.compareBtn : // CompareButton
 
-        Button compareBtn = (Button) findViewById(id.compareBtn);
-        compareBtn.setEnabled(false);
+                EditText text_FirstKeyword = null;
+                text_FirstKeyword = (EditText) findViewById(id.firstKey);
+                EditText text_SecondKeyword = null;
+                text_SecondKeyword = (EditText) findViewById(id.secondKey);
 
-        this.searchResult = new SearchResult(firstKeyword, secondkeyword);
-        this.searchResult.addObserver(this);
-        this.searchResult.startSearch();
+                String firstKeyword = text_FirstKeyword.getText().toString();
+                String secondkeyword = text_SecondKeyword.getText().toString();
+
+                Button compareBtn = (Button) findViewById(id.compareBtn);
+                compareBtn.setEnabled(false);
+
+                this.searchResult = new SearchResult(firstKeyword, secondkeyword);
+                this.searchResult.addObserver(this);
+                this.searchResult.startSearch();
+
+                break;
+        }
+
+
+        /////////////////////////////////
     }
+
 
 
     @Override
@@ -257,6 +319,11 @@ public class MainActivity extends Activity implements Observer {
 //                + " : " + String.valueOf(this.searchResult.getResult1()) + " VS " +
 //                String.valueOf(this.searchResult.getResult2()));
 
+        MAinDialog1 mDialog = new MAinDialog1(this.searchResult.getKeyword1(),
+                this.searchResult.getKeyword2(),
+                this.searchResult.getResult1(),
+                this.searchResult.getResult2());
+        mDialog.show(getFragmentManager(), "Compare");
 
 
         this.searchResult.deleteObserver(this);
@@ -264,4 +331,47 @@ public class MainActivity extends Activity implements Observer {
         Button compareBtn = (Button) findViewById(id.compareBtn);
         compareBtn.setEnabled(true);
     }
+
+
+    // LongClick Dialog
+    public static class LIstviewDialog extends DialogFragment{
+        String f1;
+        String f2;
+        int re1;
+        int re2;
+
+    }
+
+    //Dialog
+
+    public static class MAinDialog1 extends DialogFragment{
+        String f1;
+        String f2;
+        int re1;
+        int re2;
+        private LayoutInflater mLayoutInflater;
+
+        public MAinDialog1(String first, String second, int r1, int r2){
+            f1 = first;
+            f2 = second;
+            re1 = r1;
+            re2 = r2;
+        }
+
+        @Override
+        public android.app.Dialog onCreateDialog(Bundle savedInstanceState){
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+             mLayoutInflater = getActivity().getLayoutInflater();
+            mBuilder.setView(mLayoutInflater.inflate(layout.dialog, null));
+            mBuilder.setTitle("The Result");
+            mBuilder.setMessage(f1 + " : " + String.valueOf(re1) + " VS " + f2 + " : " + String.valueOf(re2));
+            return mBuilder.create();
+        }
+        @Override
+        public void onStop(){
+            super.onStop();
+        }
+    }
+
+
 }
